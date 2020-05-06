b02f4efc2abae51ded4c496d59b14196942040e4
ValadezGustavo <gustavo.valadez17@tectijuana.edu.mx>
2020-05-06 16:56:49 -0700

    P01 v2
    Actualizacion del P01 con modelos de producto y proveedor

---
+++ ControlInventario2/ControlInventario/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ControlInventario.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int ProductCode { get; set; }
+        public int ProductName { get; set; }
+        public int Description { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+
+        public int SupplierId { get; set; }
+        [ForeignKey("SupplierId")]
+        public Supplier Supplier { get; set; }
+      
+    }
+}
