291fc07b968455b0dc8cd350b324437ad1dc5a00
ValadezGustavo <gustavo.valadez17@tectijuana.edu.mx>
2020-05-12 22:23:43 -0700

    P04 - ProductBySupplier & Views

---
+++ ControlInventario4/ControlInventario/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControlInventario.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Display(Name="Código")]//Data Annotation
+        public int ProductCode { get; set; }
+        [Display(Name = "Nombre")]//Data Annotation
+        public string ProductName { get; set; }
+        [Display(Name = "Descripción")]//Data Annotation
+        public string Description { get; set; }
+        [Display(Name = "Cantidad")]//Data Annotation
+        public int Quantity { get; set; }
+        [Display(Name = "Precio")]//Data Annotation
+        public int Price { get; set; }
+
+        public int SupplierId { get; set; }
+        [ForeignKey("SupplierId")]
+        public Supplier Supplier { get; set; }
+      
+    }
+}
---
+++ ControlInventario4/ControlInventario/Models/Supplier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ControlInventario.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        [Display(Name = "Código Proveedor")]//Data Annotation
+        public int SupplierCode { get; set; }
+        [Display(Name = "Proveedor")]//Data Annotation
+        public string SupplierName { get; set; }
+        [Display(Name = "Correo")]//Data Annotation
+        public string Email { get; set; }
+        [Display(Name = "Teléfono")]//Data Annotation
+        public string Phone { get; set; }
+        [Display(Name = "Dirección")]//Data Annotation
+        public string Address { get; set; }
+
+
+    }
+}
